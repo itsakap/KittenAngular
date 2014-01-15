2c63b291af576dcb373ba6d8a3556e39dbb0a312
Adam Kaplan <AdamHKaplan@gmail.com>
2014-01-15 10:56:26 -0800

    created pet objects and connected from ctrl to view

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function KittensCtrl($scope,$window){
-  $scope.pets = ["Dr. Doom","Wilson","Tuna"];
-  $scope.test = function(){
-    $window.alert('hieeeee');
+  $scope.pets = [
+  {name: "Dr. Doom", image: "http://placekitten.com/150/150",adopted:false},{name: "Wilson", image: "http://placekitten.com/151/150",adopted:false},{name: "Tuna", image: "http://placekitten.com/152/150",adopted:false}];
+  $scope.test = function(petname){
+    $window.alert(petname + ' says: "I want you to adopt me, momma."');
   }
 }

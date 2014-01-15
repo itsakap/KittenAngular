4085d18bac411a78ff99e94882320e3f3f1a04b2
Adam Kaplan <AdamHKaplan@gmail.com>
2014-01-15 11:59:18 -0800

    added add pet functionality

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function KittensCtrl($scope,$window){
   $scope.pets = [
   {name: "Dr. Doom", image: "http://placekitten.com/150/150",adopted:false},{name: "Wilson", image: "http://placekitten.com/151/150",adopted:false},{name: "Tuna", image: "http://placekitten.com/152/150",adopted:false}];
   $scope.test = function(petname){
-    $window.alert(petname + ' says: "I want you to adopt me, momma."');
+    alert(petname + ' says:'+ ((petname=="Lumpy Space Princess") ? ' "ohmagash"' : ' "I want you to adopt me, momma."'));
+  }
+  $scope.addPet = function(){
+    $scope.pets.push({name:$scope.newName, image:$scope.newImgUrl,adopted:false});
+    $scope.newName="";
+    $scope.newImgUrl="";
   }
 }
